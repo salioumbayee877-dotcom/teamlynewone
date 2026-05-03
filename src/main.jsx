ab38f445a44030f9cf1cf600b3e56d44e9516f4d
Serigne Saliou <salioumbayee877@gmail.com>
2026-05-03 16:59:55 +0200

    Activar Sentry SDK

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import * as Sentry from '@sentry/react'
 import App from './App'
+
+Sentry.init({
+  dsn: "https://f9ab4ebc622cca0a77e4227c91389f06@o4511325827760128.ingest.de.sentry.io/4511326170972240",
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.replayIntegration(),
+  ],
+  tracesSampleRate: 1.0,
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1.0,
+})
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
